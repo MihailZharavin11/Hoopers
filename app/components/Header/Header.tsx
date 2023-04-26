86b1b34ff8db61d9872b9d5abd7fa50ec99fea95
Mikhail <mihail.zharavin65@gmail.com>
2023-04-26 20:19:25 +0300

    Deleted import ImageSVG

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import styles from "./header.module.scss";
 import Image from "next/image";
 import { Navigation } from "./Navigation/Navigation";
-import ImageSvg from "../ImageSVG/ImageSvg";
 
 const Header = () => {
   return (
