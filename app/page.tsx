4997267bbe1e17765e3689cc2bce2b215b2b8a01
Mikhail <mihail.zharavin65@gmail.com>
2023-05-01 20:03:26 +0300

    renamed folder with components
    Renamed home to homeComponents

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import GameSection from "./components/home/GamesSection/GameSection";
-import PhotoSection from "./components/home/PhotoSection/PhotoSection";
-import PlayersSection from "./components/home/PlayersSection/PlayersSection";
-import TeamsSection from "./components/home/TeamsSection/TeamsSection";
+import GameSection from "./components/homeComponents/GamesSection/GameSection";
+import PhotoSection from "./components/homeComponents/PhotoSection/PhotoSection";
+import PlayersSection from "./components/homeComponents/PlayersSection/PlayersSection";
+import TeamsSection from "./components/homeComponents/TeamsSection/TeamsSection";
 
 export default function Home() {
   return (
